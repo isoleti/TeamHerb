b349d3a233fc00a24014dca18f5fdb024b67fa1c
isoleti <bmn10101@gmail.com>
2023-02-09 21:31:52 +0900

    센터정보및 상담사 쿼리추가

--- TeamHub/src/main/java/project/healingcamp/dao/ReserveDAO.java
+++ TeamHub/src/main/java/project/healingcamp/dao/ReserveDAO.java
@@ -1,6 +1,7 @@
 package project.healingcamp.dao;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -15,20 +16,32 @@ public class ReserveDAO {
 	 @Autowired
 	 private SqlSession sqlSession;
 	 
+	 //현장결제예약 insert
 	 public int resF(ReserveVO vo) {
 	 
 		 return sqlSession.insert("project.healingcamp.mapper.reserveMapper.resF", vo);
 	 }
 	 
+	 //결제완료 insert
 	 public int couF(ReserveVO vo) {
 		 
 		 return sqlSession.insert("project.healingcamp.mapper.reserveMapper.couF", vo);
 	 }
 	 
+	 //center key값찾기
 	 public ReserveVO cnoidx(ReserveVO vo) {
 		 
 		 return sqlSession.selectOne("project.healingcamp.mapper.reserveMapper.cnoidx",vo);
 	 }
 	 
+	 //상담사 key값찾기
+	 public ReserveVO conidx(ReserveVO vo) {
+		 
+		 return sqlSession.selectOne("project.healingcamp.mapper.reserveMapper.conidx" ,vo);
+	 }
 	 
+	 public List centerlist(ReserveVO vo) {
+		 
+		 return sqlSession.selectList("project.healingcamp.mapper.reserveMapper.centerlist" ,vo);
+	 }
 }
